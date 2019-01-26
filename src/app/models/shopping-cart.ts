5b0dd1384393f494220b093aa3477115868efd80
Miley C <mileycao919@gmail.com>
2019-01-26 18:55:11 -0500

    Ceated "Clear Shopping Cart" function
    -At "Shopping Cart" Page

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -11,12 +11,13 @@ export class ShoppingCart{
           //this.items.push(itemsMap[productId]);
           this.items.push(new ShoppingCartItem(item.product, item.quantity))
       }*/
-      
+        
       this.itemsMap = itemsMap || {};
 
         for (let productId in itemsMap) {
         let item = itemsMap[productId];
         let x = new ShoppingCartItem();
+         
         Object.assign(x, item);
         x.key = productId; 
         this.items.push(x);
